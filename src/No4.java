6dbb5438d7cdc93ec1efb90944745758bd19da61
Sanghyeok Park <suran5281@gmail.com>
2024-03-21 13:32:35 +0900

    No.4 단어 뒤집기

--- src/No4.java
+++ src/No4.java
@@ -1,20 +1,54 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * 4. 단어 뒤집기
+ * Q. N개의 단어가 주어지면 각 단어를 뒤집어 출력하는 프로그램을 작성하세요.
+ */
 public class No4 {
     public static void main(String[] args){
         Scanner in=new Scanner(System.in);
-        int input = in.nextInt();
-        ArrayList<String> strs = new ArrayList<>();
-        for(int i=0; i<input; i++){
-            String str = in.nextLine();
-            strs.add(str);
+        int count = in.nextInt();
+        String[] words = new String[count];
+        for(int i=0; i<count; i++){
+            words[i] = in.next();
         }
 
-        System.out.println(solution(input));
+        for(String word : solution(count, words)){
+            System.out.println(word);
+        }
     }
 
-    public static void solution(ArrayList<String> strs){
+    public static ArrayList<String> solution(int count, String[] words){
+        ArrayList<String> answer = new ArrayList<>();
 
+        //----------------------------------------------------
+        // 1. StringBuilder의 reverse 메서드를 이용
+//        for(String word : words){
+//            String tmp = new StringBuilder(word).reverse().toString();
+//            answer.add(tmp);
+//        }
+        //----------------------------------------------------
+
+        //----------------------------------------------------
+        // 2. 하나씩 자리 바꾸기
+        for(String word : words){
+            char[] chars = word.toCharArray(); // 문자배열
+            int lt = 0;
+            int rt = word.length()-1;
+
+            // lt, rt를 교환하고 lt는 +1, rt는 -1자리로 이동
+            while(lt<rt){
+                char tmp = chars[lt];
+                chars[lt] = chars[rt];
+                chars[rt] = tmp;
+                lt++;
+                rt--;
+            }
+            String tmp = String.valueOf(chars);
+            answer.add(tmp);
+        }
+        //----------------------------------------------------
+        return answer;
     }
 }
